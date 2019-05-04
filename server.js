d14dcd765c30f0eac2ab2d9171987e66ea1bf5ec
Deepcie <44144294+deepcie@users.noreply.github.com>
2019-05-04 12:24:18 -0700

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-// Node Server code
-var express = require('express');
-var app = express();
-
-var port = 8001;
-app.use(express.static(__dirname + '/'));
-app.listen(port);
-console.log('Server started at : ' + port);
